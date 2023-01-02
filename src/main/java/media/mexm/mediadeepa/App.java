cfd740b351754c8103ee51fd9139d163f5486d76
hdsdi3g <admin@hd3g.tv>
2023-01-02 11:50:52 +0100

    fixup java project

---
+++ src/main/java/media/mexm/mediadeepa/App.java
@@ -0,0 +1,29 @@
+/*
+ * This file is part of mediadeepa.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * Copyright (C) Media ex Machina 2023
+ *
+ */
+package media.mexm.mediadeepa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class App {
+
+	public static void main(final String[] args) {
+		SpringApplication.run(App.class, args);
+	}
+
+}
---
+++ src/test/java/media/mexm/mediadeepa/CodePolicyValidationTest.java
@@ -0,0 +1,22 @@
+/*
+ * This file is part of weblivegraphics.
+ *
+ * weblivegraphics is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * any later version.
+ *
+ * weblivegraphics is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * Copyright (C) Media ex Machina 2023
+ *
+ */
+package media.mexm.mediadeepa;
+
+import tv.hd3g.commons.codepolicyvalidation.CheckPolicy;
+
+public class CodePolicyValidationTest extends CheckPolicy {
+}
