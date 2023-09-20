9f6f565f8fa14a354a8511521f48be0fd0cfe460
hdsdi3g <admin@hd3g.tv>
2023-09-21 01:59:02 +0200

    fixup! Create graphical views for reports, as Export format #8

--- src/main/java/media/mexm/mediadeepa/exportformat/graphic/LoudnessGraphicExportFormat.java
+++ src/main/java/media/mexm/mediadeepa/exportformat/graphic/LoudnessGraphicExportFormat.java
@@ -18,44 +18,29 @@ package media.mexm.mediadeepa.exportformat.graphic;
 
 import static java.awt.BasicStroke.CAP_BUTT;
 import static java.awt.BasicStroke.JOIN_MITER;
-import static java.awt.Color.BLACK;
 import static java.awt.Color.BLUE;
-import static java.awt.Color.CYAN;
-import static java.awt.Color.GRAY;
 import static java.awt.Color.ORANGE;
 import static java.awt.Color.RED;
-import static java.lang.Math.max;
 import static media.mexm.mediadeepa.components.CLIRunner.makeOutputFileName;
 
 import java.awt.BasicStroke;
-import java.awt.Font;
+import java.awt.Color;
+import java.awt.Point;
 import java.io.File;
-import java.io.IOException;
-import java.io.UncheckedIOException;
-import java.util.TimeZone;
 import java.util.stream.Stream;
 
-import org.jfree.chart.ChartFactory;
-import org.jfree.chart.ChartUtilities;
-import org.jfree.chart.axis.DateAxis;
-import org.jfree.chart.axis.LogarithmicAxis;
-import org.jfree.chart.plot.ValueMarker;
-import org.jfree.chart.renderer.xy.XYLineAndShapeRenderer;
-import org.jfree.data.time.FixedMillisecond;
-import org.jfree.data.time.TimeSeries;
-import org.jfree.data.time.TimeSeriesCollection;
-
-import lombok.extern.slf4j.Slf4j;
 import media.mexm.mediadeepa.exportformat.DataResult;
 import media.mexm.mediadeepa.exportformat.ExportFormat;
+import media.mexm.mediadeepa.exportformat.graphic.TimedDataGraphic.RangeAxis;
 import tv.hd3g.fflauncher.recipes.MediaAnalyserResult;
 import tv.hd3g.fflauncher.resultparser.Ebur128StrErrFilterEvent;
 import tv.hd3g.fflauncher.resultparser.Ebur128Summary;
+import tv.hd3g.fflauncher.resultparser.Stereo;
 
-@Slf4j
 public class LoudnessGraphicExportFormat implements ExportFormat {
 
-	public static final String SUFFIX_FILE_NAME = "lufs-events.jpg";
+	public static final String SUFFIX_LUFS_FILE_NAME = "lufs-events.jpg";
+	public static final String SUFFIX_TPK_FILE_NAME = "lufs-tpk-events.jpg";
 
 	@Override
 	public String getFormatLongName() {
@@ -68,172 +53,84 @@ public class LoudnessGraphicExportFormat implements ExportFormat {
 		if (r128events.isEmpty()) {
 			return;
 		}
-		// TODO polyvalent time/db (LogarithmicAxis or other ?) values graph
-		// TODO split TPK / LU
 
-		final var outputFile = new File(exportDirectory, makeOutputFileName(baseFileName, SUFFIX_FILE_NAME));
-		final var jpg_compression_ratio = 0.95f;
-		final var image_width = 1000 * 2;
-		final var image_height = 600 * 2;
-		final var truepeakRef = -3;
-
-		final var seriesMomentary = new TimeSeries("Momentary");
-		final var seriesShortTerm = new TimeSeries("Short term");
-		final var seriesIntegrated = new TimeSeries("Integrated");
-		final var seriesTruePeakPerFrame = new TimeSeries("True peak");
-
-		r128events.forEach(event -> {
-			final var position = new FixedMillisecond(Math.round(Math.ceil(event.getT() * 1000f)));
-			seriesMomentary.add(position, event.getM());
-			seriesShortTerm.add(position, event.getS());
-			seriesIntegrated.add(position, event.getI());
-			seriesTruePeakPerFrame.add(position, max(event.getFtpk().left(), event.getFtpk().right()));
-		});
-
-		final var tsc = new TimeSeriesCollection();
-		tsc.addSeries(seriesShortTerm);
-		tsc.addSeries(seriesMomentary);
-		tsc.addSeries(seriesTruePeakPerFrame);
-		tsc.addSeries(seriesIntegrated);
-
-		final var timechart = ChartFactory.createTimeSeriesChart("", "", "", tsc, true, false, false);
-		timechart.setAntiAlias(true);
-		timechart.setBackgroundPaint(BLACK);
-		final var legend = timechart.getLegend();
-		legend.setBackgroundPaint(BLACK);
-
-		final var plot = timechart.getXYPlot();
-		plot.setBackgroundPaint(BLACK);
-
-		final var renderer = (XYLineAndShapeRenderer) plot.getRenderer(0);
-		renderer.setBaseLegendTextPaint(GRAY);
-
-		final var font = renderer.getBaseItemLabelFont().deriveFont(24f);
-		renderer.setBaseItemLabelFont(font);
-		renderer.setBaseLegendTextFont(font);
-		renderer.setLegendTextFont(0, font);
-		renderer.setSeriesItemLabelFont(0, font);
+		final var dataGraphicLUFS = new TimedDataGraphic(
+				r128events.stream()
+						.map(Ebur128StrErrFilterEvent::getT)
+						.map(t -> t * 1000f)
+						.map(Math::ceil)
+						.map(Math::round),
+				RangeAxis.createFromValueSet("dB LU", -40, 10, 1,
+						r128events.stream()
+								.flatMap(event -> Stream.of(
+										event.getI(),
+										event.getM(),
+										event.getS(),
+										event.getTarget()))));
 
 		final var thinStroke = new BasicStroke(3, CAP_BUTT, JOIN_MITER);
-		/**
-		 * series_short_term
-		 */
-		renderer.setSeriesPaint(0, ORANGE);
-		renderer.setSeriesStroke(0, thinStroke);
-		/**
-		 * series_momentary
-		 */
-		renderer.setSeriesPaint(1, RED);
-		renderer.setSeriesStroke(1, thinStroke);
-		/**
-		 * series_true_peak_per_frame
-		 */
-		renderer.setSeriesPaint(2, GRAY);
-		renderer.setSeriesStroke(2, thinStroke);
-		/**
-		 * series_integrated
-		 */
-		renderer.setSeriesPaint(3, BLUE);
-		renderer.setSeriesStroke(3, new BasicStroke(6, CAP_BUTT, JOIN_MITER));
+		final var thickStroke = new BasicStroke(6, CAP_BUTT, JOIN_MITER);
 
-		/**
-		 * Time units
-		 */
-		final var timeAxis = (DateAxis) plot.getDomainAxis();
-		timeAxis.setAxisLineVisible(false);
-		timeAxis.setLabelPaint(GRAY);
-		timeAxis.setTickLabelPaint(GRAY);
-		timeAxis.setLowerMargin(0);
-		timeAxis.setUpperMargin(0);
-		timeAxis.setTimeZone(TimeZone.getTimeZone("GMT"));
-		timeAxis.setMinorTickMarksVisible(true);
-		timeAxis.setMinorTickCount(10);
-		timeAxis.setMinorTickMarkInsideLength(5);
-		timeAxis.setMinorTickMarkOutsideLength(0);
-		timeAxis.setLabelFont(font);
-		timeAxis.setTickLabelFont(font);
-
-		final var floorValuesStats = -40d;
-		final var valueStats = r128events.stream().flatMap(event -> Stream.of(
-				event.getFtpk().left(),
-				event.getFtpk().right(),
-				event.getI(),
-				event.getM(),
-				event.getS(),
-				event.getSpk().left(),
-				event.getSpk().right(),
-				event.getTarget(),
-				event.getTpk().left(),
-				event.getTpk().right()))
-				.mapToDouble(v -> (double) v)
-				.filter(v -> v > floorValuesStats)
-				.summaryStatistics();
-		final var maxValue = (int) Math.round(Math.floor(valueStats.getMax()));
-		final var minValue = (int) Math.round(Math.ceil(valueStats.getMin()));
+		dataGraphicLUFS.addSeries(dataGraphicLUFS.new Series(
+				"Momentary", ORANGE, thinStroke,
+				r128events.stream().map(Ebur128StrErrFilterEvent::getM)));
+		dataGraphicLUFS.addSeries(dataGraphicLUFS.new Series(
+				"Short term", RED, thinStroke,
+				r128events.stream().map(Ebur128StrErrFilterEvent::getS)));
+		dataGraphicLUFS.addSeries(dataGraphicLUFS.new Series(
+				"Integrated", BLUE, thickStroke,
+				r128events.stream().map(Ebur128StrErrFilterEvent::getI)));
 
 		final var oEbur128Sum = result.getMediaAnalyserResult().map(MediaAnalyserResult::ebur128Summary);
-		final var integrated = Math.round(oEbur128Sum.map(Ebur128Summary::getIntegrated)
-				.orElseGet(() -> r128events.get(r128events.size() - 1).getI()));
-		final var lraH = oEbur128Sum.map(Ebur128Summary::getLoudnessRangeHigh)
-				.map(Math::floor)
-				.map(Math::round)
-				.orElse(-23l);
-		final var lraL = oEbur128Sum.map(Ebur128Summary::getLoudnessRangeLow)
-				.map(Math::ceil)
-				.map(Math::round)
-				.orElse(-23l);
+		dataGraphicLUFS
+				.addValueMarker(oEbur128Sum.map(Ebur128Summary::getIntegrated))
+				.addValueMarker(oEbur128Sum.map(Ebur128Summary::getLoudnessRangeHigh))
+				.addValueMarker(oEbur128Sum.map(Ebur128Summary::getLoudnessRangeLow))
+				.addValueMarker(r128events.get(r128events.size() - 1).getI());
 
-		final var rangeAxis = new LogarithmicAxis("dB LU");
-		rangeAxis.centerRange(integrated);
-		rangeAxis.setAllowNegativesFlag(true);
-		rangeAxis.setRange(minValue - 10d, maxValue + 1d);
-		rangeAxis.setLabelPaint(GRAY);
-		rangeAxis.setAxisLineVisible(false);
-		rangeAxis.setTickLabelPaint(GRAY);
-		rangeAxis.setLabelFont(font);
+		dataGraphicLUFS.makeGraphic(
+				new File(exportDirectory, makeOutputFileName(baseFileName, SUFFIX_LUFS_FILE_NAME)),
+				new Point(2000, 1200));
 
-		rangeAxis.setTickLabelFont(font);
-		rangeAxis.setTickLabelsVisible(true);
-		rangeAxis.setTickMarksVisible(true);
-		rangeAxis.setTickMarkInsideLength(10);
-		rangeAxis.setTickMarkOutsideLength(0);
-		rangeAxis.setTickMarkPaint(GRAY);
-
-		plot.setRangeAxis(rangeAxis);
-		plot.setOutlinePaint(GRAY);
-
-		Stream.of(
+		final var ra = RangeAxis.createFromValueSet("dB LU",
+				oEbur128Sum.map(Ebur128Summary::getIntegrated).orElse(-23f), 10, 1,
 				r128events.stream()
-						.map(Ebur128StrErrFilterEvent::getTarget)
-						.map(Math::round)
-						.findFirst()
-						.orElse(-23),
-				-14, -23, maxValue, minValue, integrated, lraH.intValue(), lraL.intValue())
-				.distinct()
-				.map(ref -> createValueMarker(ref, font))
-				.forEach(plot::addRangeMarker);
-		plot.addRangeMarker(createValueMarker(truepeakRef, font));
+						.flatMap(event -> Stream.of(
+								event.getFtpk().left(),
+								event.getFtpk().right(),
+								event.getSpk().left(),
+								event.getSpk().right())));
+		final var dataGraphicTPK = dataGraphicLUFS.cloneWithSamePositions(ra);
 
-		timechart.setTextAntiAlias(true);
+		dataGraphicTPK.addSeries(dataGraphicLUFS.new Series(
+				"True peak left (per frame)", BLUE, thinStroke,
+				r128events.stream().map(Ebur128StrErrFilterEvent::getFtpk).map(Stereo::left)));
+		dataGraphicTPK.addSeries(dataGraphicLUFS.new Series(
+				"True peak right (per frame)", RED, thinStroke,
+				r128events.stream().map(Ebur128StrErrFilterEvent::getFtpk).map(Stereo::right)));
+		dataGraphicTPK.addSeries(dataGraphicLUFS.new Series(
+				"Sample peak left", new Color(0, 128, 255), thickStroke,
+				r128events.stream().map(Ebur128StrErrFilterEvent::getSpk).map(Stereo::left)));
+		dataGraphicTPK.addSeries(dataGraphicLUFS.new Series(
+				"Sample peak right", ORANGE, thickStroke,
+				r128events.stream().map(Ebur128StrErrFilterEvent::getSpk).map(Stereo::right)));
+		dataGraphicTPK
+				.addValueMarker(oEbur128Sum.map(Ebur128Summary::getTruePeak))
+				.addValueMarker(oEbur128Sum.map(Ebur128Summary::getSamplePeak))
+				.addValueMarker(-3)
+				.addValueMarker(r128events.stream()
+						.flatMap(event -> Stream.of(
+								event.getFtpk().left(),
+								event.getFtpk().right(),
+								event.getSpk().left(),
+								event.getSpk().right()))
+						.mapToDouble(Float::doubleValue)
+						.summaryStatistics().getAverage());
 
-		try {
-			log.info("Save LUFS graphic to {}", outputFile);
-			ChartUtilities.saveChartAsJPEG(outputFile, jpg_compression_ratio, timechart, image_width, image_height);
-		} catch (final IOException e) {
-			throw new UncheckedIOException("Can't save chart", e);
-		}
-	}
+		dataGraphicTPK.makeGraphic(
+				new File(exportDirectory, makeOutputFileName(baseFileName, SUFFIX_TPK_FILE_NAME)),
+				new Point(2000, 1200));
 
-	private ValueMarker createValueMarker(final int ref, final Font font) {
-		final var maker = new ValueMarker(ref);
-		maker.setLabel("   " + ref);
-		maker.setLabelPaint(CYAN);
-		maker.setPaint(CYAN);
-		maker.setOutlinePaint(CYAN);
-		maker.setLabelFont(font);
-		final float[] dash = { 5.0f };
-		maker.setStroke(new BasicStroke(1, CAP_BUTT, JOIN_MITER, 10.0f, dash, 0.0f));
-		return maker;
 	}
 
 }
---
+++ src/main/java/media/mexm/mediadeepa/exportformat/graphic/TimedDataGraphic.java
@@ -0,0 +1,257 @@
+/*
+ * This file is part of mediadeepa.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * Copyright (C) Media ex Machina 2023
+ *
+ */
+package media.mexm.mediadeepa.exportformat.graphic;
+
+import static java.awt.BasicStroke.CAP_BUTT;
+import static java.awt.BasicStroke.JOIN_MITER;
+import static java.awt.Color.BLACK;
+import static java.awt.Color.CYAN;
+import static java.awt.Color.GRAY;
+import static java.util.Locale.ENGLISH;
+
+import java.awt.BasicStroke;
+import java.awt.Font;
+import java.awt.Paint;
+import java.awt.Point;
+import java.awt.Stroke;
+import java.io.File;
+import java.io.IOException;
+import java.io.UncheckedIOException;
+import java.text.DecimalFormat;
+import java.text.DecimalFormatSymbols;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.TimeZone;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartUtilities;
+import org.jfree.chart.axis.DateAxis;
+import org.jfree.chart.axis.LogarithmicAxis;
+import org.jfree.chart.plot.ValueMarker;
+import org.jfree.chart.renderer.xy.XYLineAndShapeRenderer;
+import org.jfree.data.time.FixedMillisecond;
+import org.jfree.data.time.TimeSeries;
+import org.jfree.data.time.TimeSeriesCollection;
+import org.jfree.ui.TextAnchor;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class TimedDataGraphic { // TODO test ? E2E ?
+
+	private static final float JPG_COMPRESSION_RATIO = 0.95f;
+	private static final String DECIMAL_FORMAT_PATTERN = "#.#";
+
+	private final List<Series> series;
+	private final List<Double> markers;
+	private final List<FixedMillisecond> positions;
+	private final RangeAxis rangeAxis;
+	private final DecimalFormat decimalFormat;
+
+	public TimedDataGraphic(final Stream<Long> positionsMs, final RangeAxis rangeAxis) {
+		series = new ArrayList<>();
+		markers = new ArrayList<>();
+		positions = positionsMs.map(FixedMillisecond::new).toList();
+		this.rangeAxis = Objects.requireNonNull(rangeAxis, "\"rangeAxis\" can't to be null");
+
+		decimalFormat = new DecimalFormat(DECIMAL_FORMAT_PATTERN);
+		decimalFormat.setDecimalFormatSymbols(new DecimalFormatSymbols(ENGLISH));
+	}
+
+	private TimedDataGraphic(final TimedDataGraphic reference, final RangeAxis rangeAxis) {
+		series = new ArrayList<>();
+		markers = new ArrayList<>();
+		positions = reference.positions;
+		this.rangeAxis = rangeAxis;
+
+		decimalFormat = new DecimalFormat(DECIMAL_FORMAT_PATTERN);
+		decimalFormat.setDecimalFormatSymbols(new DecimalFormatSymbols(ENGLISH));
+	}
+
+	public TimedDataGraphic addValueMarker(final Number position) {
+		final var doublePos = position.doubleValue();
+		if (markers.contains(doublePos) == false) {
+			markers.add(doublePos);
+		}
+		return this;
+	}
+
+	public TimedDataGraphic addValueMarker(final Optional<Number> position) {
+		position.ifPresent(this::addValueMarker);
+		return this;
+	}
+
+	public TimedDataGraphic addSeries(final Series s) {
+		Objects.requireNonNull(s);
+		series.add(s);
+		return this;
+	}
+
+	public TimedDataGraphic cloneWithSamePositions(final RangeAxis rangeAxis) {
+		return new TimedDataGraphic(this, rangeAxis);
+	}
+
+	public static record RangeAxis(String name, Number min, Number max) {
+
+		public static <T extends Number> RangeAxis createFromValueSet(final String name,
+																	  final Number floor,
+																	  final Number subMarginMin,
+																	  final Number addMarginMax,
+																	  final Stream<T> values) {
+			final var doubleFloor = floor.doubleValue();
+			final var stats = values.mapToDouble(Number::doubleValue)
+					.filter(v -> v > doubleFloor)
+					.summaryStatistics();
+			return new RangeAxis(
+					name,
+					stats.getMin() - subMarginMin.doubleValue(),
+					stats.getMax() + addMarginMax.doubleValue());
+		}
+
+		private LogarithmicAxis toLogarithmicAxis(final Font font) {
+			final var rangeAxis = new LogarithmicAxis(name);
+			// rangeAxis.centerRange(integrated);
+			rangeAxis.setAllowNegativesFlag(true);
+			rangeAxis.setRange(min.doubleValue(), max.doubleValue());
+			rangeAxis.setLabelPaint(GRAY);
+			rangeAxis.setAxisLineVisible(false);
+			rangeAxis.setTickLabelPaint(GRAY);
+			rangeAxis.setLabelFont(font);
+
+			rangeAxis.setTickLabelFont(font);
+			rangeAxis.setTickLabelsVisible(true);
+			rangeAxis.setTickMarksVisible(true);
+			rangeAxis.setTickMarkInsideLength(10);
+			rangeAxis.setTickMarkOutsideLength(0);
+			rangeAxis.setTickMarkPaint(GRAY);
+			return rangeAxis;
+		}
+
+	}
+
+	public class Series {
+		private final String name;
+		private final Paint paint;
+		private final Stroke stroke;
+		private final List<Float> datas;
+
+		public Series(final String name,
+					  final Paint paint,
+					  final Stroke stroke,
+					  final Stream<? extends Number> datas) {
+			this.name = Objects.requireNonNull(name, "\"name\" can't to be null");
+			this.paint = Objects.requireNonNull(paint, "\"paint\" can't to be null");
+			this.stroke = Objects.requireNonNull(stroke, "\"stroke\" can't to be null");
+
+			this.datas = datas.map(Number::floatValue).toList();
+			if (positions.size() != this.datas.size()) {
+				throw new IllegalArgumentException(
+						"Invalid dataset size (" + this.datas.size() + "), expect " + positions.size());
+			}
+		}
+
+		private TimeSeries getTimeSeries() {
+			final var ts = new TimeSeries(name);
+			IntStream.range(0, datas.size())
+					.forEach(pos -> ts.add(positions.get(pos), datas.get(pos)));
+			return ts;
+		}
+
+	}
+
+	public void makeGraphic(final File outputFile, final Point imageSize) {
+		log.info("Save graphic to {}", outputFile);
+
+		final var tsc = new TimeSeriesCollection();
+		series.stream()
+				.map(Series::getTimeSeries)
+				.forEach(tsc::addSeries);
+
+		final var timechart = ChartFactory.createTimeSeriesChart("", "", "", tsc, true, false, false);
+		timechart.setAntiAlias(true);
+		timechart.setTextAntiAlias(true);
+		timechart.setBackgroundPaint(BLACK);
+		final var legend = timechart.getLegend();
+		legend.setBackgroundPaint(BLACK);
+
+		final var plot = timechart.getXYPlot();
+		plot.setBackgroundPaint(BLACK);
+		plot.setOutlinePaint(GRAY);
+
+		final var renderer = (XYLineAndShapeRenderer) plot.getRenderer(0);
+		renderer.setBaseLegendTextPaint(GRAY);
+
+		final var font = renderer.getBaseItemLabelFont().deriveFont(24f);
+		markers.forEach(ref -> {
+			final var marker = new ValueMarker(ref);
+			final var label = decimalFormat.format(ref);
+			if (label.endsWith(".0")) {
+				marker.setLabel(Math.round(ref) + "");
+			} else {
+				marker.setLabel(label);
+			}
+
+			marker.setLabelPaint(CYAN);
+			marker.setPaint(CYAN);
+			marker.setOutlinePaint(CYAN);
+			marker.setLabelFont(font);
+			marker.setLabelTextAnchor(TextAnchor.TOP_LEFT);
+			final float[] dash = { 5.0f };
+			marker.setStroke(new BasicStroke(1, CAP_BUTT, JOIN_MITER, 10.0f, dash, 0.0f));
+			plot.addRangeMarker(marker);
+		});
+
+		plot.setRangeAxis(rangeAxis.toLogarithmicAxis(font));
+
+		renderer.setBaseItemLabelFont(font);
+		renderer.setBaseLegendTextFont(font);
+		renderer.setLegendTextFont(0, font);
+		renderer.setSeriesItemLabelFont(0, font);
+
+		for (var pos = 0; pos < series.size(); pos++) {
+			final var s = series.get(pos);
+			renderer.setSeriesPaint(pos, s.paint);
+			renderer.setSeriesStroke(pos, s.stroke);
+		}
+
+		final var timeAxis = (DateAxis) plot.getDomainAxis();
+		timeAxis.setAxisLineVisible(false);
+		timeAxis.setLabelPaint(GRAY);
+		timeAxis.setTickLabelPaint(GRAY);
+		timeAxis.setLowerMargin(0);
+		timeAxis.setUpperMargin(0);
+		timeAxis.setTimeZone(TimeZone.getTimeZone("GMT"));
+		timeAxis.setMinorTickMarksVisible(true);
+		timeAxis.setMinorTickCount(10);
+		timeAxis.setMinorTickMarkInsideLength(5);
+		timeAxis.setMinorTickMarkOutsideLength(0);
+		timeAxis.setLabelFont(font);
+		timeAxis.setTickLabelFont(font);
+
+		try {
+			ChartUtilities.saveChartAsJPEG(outputFile, JPG_COMPRESSION_RATIO, timechart, imageSize.x, imageSize.y);
+		} catch (final IOException e) {
+			throw new UncheckedIOException("Can't save chart", e);
+		}
+
+	}
+
+}
